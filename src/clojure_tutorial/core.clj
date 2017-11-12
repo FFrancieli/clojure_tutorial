dedf01ffa4dcf2ce2c48a681a0a0280f1971f87d
Francieli Viane <francielifrv@gmail.com>
2017-11-12 17:51:16 +0100

    implements ring handlers

--- src/clojure_tutorial/core.clj
+++ src/clojure_tutorial/core.clj
@@ -1,6 +1,10 @@
 (ns clojure-tutorial.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn example-handler [request]
+  {:body "Hello clojure!"})
+
+(defn on-init[]
+  println "Initializing sample web app...")
+
+(defn on-destroy []
+  println "destroying sample web app...")
